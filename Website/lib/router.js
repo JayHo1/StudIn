11b290a196664eecc8a86c4f7d17e534abf7c97a
Jay Chau HO <JayChau@iMac-de-Jay.local>
2015-01-27 00:40:56 +0100

    web1

--- Website/lib/router.js
+++ Website/lib/router.js
@@ -2,10 +2,14 @@ Router.configure({
 	layoutTemplate: 'layout'
 });
 
-Router.route('/entreprises', function(){
+Router.route('/entreprise', function(){
 	this.render('entreprise');
 });
 
 Router.route('/', function(){
 	this.render('first-page');
+});
+
+Router.route('/ecole', function(){
+	this.render('ecole');
 });
